0b6c0fe04564d9e2317dd015b0db3b8a69d8cc55
ryousuf569 <yousufrashid.coding@gmail.com>
2026-07-25 18:40:26 -0600

    ball pass action decoder

--- engine.py
+++ engine.py
@@ -4,6 +4,7 @@ from schema import player_dt
 V_MAX = 5.0
 A_MAX = 7.0
 DT = 0.1
+HOLD = 0
 
 offset = np.array([43.0, 0.0])
 root_2 = np.sqrt(2)
@@ -32,8 +33,27 @@ def action_decoding(direction_idx, speed_idx):
     target_directions = direction_lookup[direction_idx]
     target_speeds = speed_lookup[speed_idx]
     target_velocities = target_directions * target_speeds[:, np.newaxis]
-    
+
     return target_velocities
+
+def ball_action(ball_idx, holder_id, player_ids):
+
+    holder_is_attacker = holder_id is not None and np.any(player_ids == holder_id)
+
+    if holder_is_attacker:
+        ball_idx = np.where(player_ids == holder_id, ball_idx, HOLD)
+        holder_choice = ball_idx[player_ids == holder_id][0]
+        is_pass = holder_choice != HOLD
+
+        if is_pass:
+            teammate_ids = np.sort(player_ids[player_ids != holder_id])
+            target_id = teammate_ids[holder_choice - 1]
+            return holder_id, True, target_id
+        else:
+            return holder_id, False, None
+    else:
+        return holder_id, False, None
+
 
 def kinematics_integrator(players, target_velocities):
 
